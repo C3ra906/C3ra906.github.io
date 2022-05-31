9c0cf3239b682c1e936ff5caccc610da47d5e3df
Cera <ceoh@pdx.edu>
2022-05-31 09:13:54 -0700

    still filling in content. playing around with design

--- about.js
+++ about.js
@@ -5,4 +5,6 @@ pic.style.height = "180px";
 pic.style.width = "180px";
 pic.style.borderRadius = "20px";
 
+let hello = document.getElementById("hello");
+hello.style.fontWeight = "bold";
 //Add to DOM
