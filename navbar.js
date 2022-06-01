08d75a5e952494d71c4020eed2e7913f889a6b05
Cera <ceoh@pdx.edu>
2022-06-01 03:07:53 -0700

    Changed color scheme, added footer to navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
+//navbar elements
 let navbar = document.querySelector("#navbar");
 
-//navbar elements
 let nav = document.createElement("nav");
 
 let btn1 = document.createElement("div");
@@ -45,46 +45,13 @@ logo.setAttribute("src", "logo.png");
 logo.setAttribute("alt", "logo C O");
 logo.setAttribute("id", "logo");
 
-//icons
-// let home = document.createElement("img");
-// home.setAttribute("src", "/icons/iconmonstr-home-7-240.png");
-// home.setAttribute("alt", "homepage");
-// home.setAttribute("href", "index.html");
-// home.setAttribute("class", "icon");
-// home.style.display = "none";
-// let person = document.createElement("img");
-// person.setAttribute("src", "/icons/iconmonstr-user-7.svg");
-// person.setAttribute("alt", "about page");
-// person.setAttribute("href", "about.html");
-// person.setAttribute("class", "icon");
-// person.style.display = "none";
-// let mail = document.createElement("img");
-// mail.setAttribute("src", "/icons/iconmonstr-home-7-240.png");
-// mail.setAttribute("alt", "homepage");
-// mail.setAttribute("href", "index.html");
-// mail.setAttribute("class", "icon");
-
-//Style
-nav.style.backgroundColor = "purple";
+//Navbar element styles
+nav.style.background = "var(--grad)";
 
 btn1.style.float = "left";
 btn2.style.float = "left";
 btn3.style.float = "left";
 btn4.style.float = "left";
-//btn5.style.float = "right";
-// btn1.style.boxSizing = "border-box";
-// btn1.style.display = "inline-block";
-// btn2.style.boxSizing = "border-box";
-// btn2.style.display = "inline-block";
-// btn3.style.boxSizing = "border-box";
-// btn3.style.display = "inline-block";
-// btn4.style.boxSizing = "border-box";
-// btn4.style.display = "inline-block";
-// btn5.style.boxSizing = "border-box";
-// btn5.style.display = "inline-block";
-
-//index.style.boxSizing = "border-box";
-//index.style.display = "inline-block";
 
 logo.style.width = "40px";
 logo.style.height = "40px";
@@ -95,7 +62,6 @@ logo.style.float = "left";
 
 index.style.float = "left";
 index.style.color = "white";
-index.style.backgroundColor = "purple";
 index.style.textDecoration = "none";
 index.style.padding = "30px";
 
@@ -103,30 +69,38 @@ about.style.boxSizing = "border-box";
 about.style.display = "inline-block";
 about.style.float = "left";
 about.style.color = "white";
-about.style.backgroundColor = "purple";
 about.style.textDecoration = "none";
 about.style.padding = "30px";
 
 previous.style.boxSizing = "border-box";
 previous.style.display = "inline-block";
 previous.style.color = "white";
-previous.style.backgroundColor = "purple";
 previous.style.textDecoration = "none";
 previous.style.padding = "30px";
 
 projects.style.boxSizing = "border-box";
 projects.style.display = "inline-block";
 projects.style.color = "white";
-projects.style.backgroundColor = "purple";
 projects.style.textDecoration = "none";
 projects.style.padding = "30px";
 
 contact.style.boxSizing = "border-box";
 contact.style.display = "inline-block";
 contact.style.color = "white";
-contact.style.backgroundColor = "purple";
 contact.style.textDecoration = "none";
 contact.style.padding = "30px";
+
+//Footer elements
+let foot = document.querySelector("#foot");
+let footer = document.createElement("p");
+footer.setAttribute("class", "footer");
+footer.innerHTML = "(c) Cera Oh 2022";
+
+//Footer element styles
+foot.style.background = "var(--grad)";
+foot.style.padding = "20px";
+footer.style.color = "var(--white)";
+foot.style.textAlign = "right";
 
 //Add to DOM
 navbar.appendChild(nav);
@@ -142,8 +116,9 @@ nav.appendChild(btn4);
 btn4.appendChild(projects);
 nav.appendChild(btn5);
 btn5.appendChild(contact);
+foot.appendChild(footer);
 
-//mouseover
+//Events
 btn1.addEventListener("mouseover", function (event) {
   index.style.fontWeight = "bold";
   index.style.color = "pink";
